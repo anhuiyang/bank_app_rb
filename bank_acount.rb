f9a9a47ff6f4688b5a027899dca6a08bc5d27aff
Hui An Yang <hecatie36@hotmail.com>
2019-02-11 11:57:48 +0000

    initial commit with 1st failing test

---
+++ bank_acount.rb
@@ -0,0 +1,18 @@
+class Account
+
+  def initialize
+    @balance = 0
+    @record = [
+  end
+
+  def deposit(credit, date)
+
+  end
+  def withdrawl
+
+  end
+  def statement
+
+  end
+
+end
---
+++ spec/bank_acount_spec.rb
@@ -0,0 +1,8 @@
+describe 'Account' do
+  let(:account) {Account.new}
+
+  it 'can deposit an amount' do
+    account.deposit(1000, 10-01-2012)
+    expect(account.balance).to eq 1000
+  end
+end
