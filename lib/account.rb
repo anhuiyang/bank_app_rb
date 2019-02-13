53c71b3cbd6dd598d29066e710e8e09db15a2c9b
Hui An Yang <hecatie36@hotmail.com>
2019-02-13 02:15:25 +0000

    unit test of account deposit/withdrawl passed

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,5 @@
 class Account
-  attr_accessor :records
+  attr_accessor :balance, :records
 
   def initialize
     @balance = 0
@@ -9,13 +9,13 @@ class Account
   def deposit(amount, date)
     @balance += amount
     log = Transaction.new(date, "credit", amount, @balance)
-    @records << new_transaction
+    @records << log
   end
 
   def withdrawl(amount, date)
-    @balance += amount
+    @balance -= amount
     log = Transaction.new(date, "debit", amount, @balance)
-    @records << new_transaction
+    @records << log
   end
 
   def statement
